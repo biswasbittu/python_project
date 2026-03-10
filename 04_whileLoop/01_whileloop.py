96688a4630a4f9f318a115c28e54e6dcb5e4a466
Bittu Biswas <biswasbittu7415@yahoo.com>
2026-03-10 12:13:57 +0530

    while loop task 2 done

--- 04_whileLoop/01_whileloop.py
+++ 04_whileLoop/01_whileloop.py
@@ -1,6 +1,21 @@
-i=int(input(""));
-while i <=5:
-    print(i)
-    i +=1
+# i=1;
+# while i <=5:
+#     print("Hellow")
+#     i +=1
+
+# i= 1;
+# total=0;
+
+# while i <= 5:
+#     total+=i;
+#     i+=1
+# print(total)
     
-    
+i=1;
+fact =1;
+
+while i <= 5 :
+    fact = fact*i;
+    i=i+1;
+    print(fact)
+print(fact)
