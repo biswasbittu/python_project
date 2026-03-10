ebca3acc3c09317eb43f50b46e97c992cb5dd9bc
Bittu Biswas <biswasbittu7415@yahoo.com>
2026-03-10 12:21:30 +0530

    while and break done

--- 04_whileLoop/01_whileloop.py
+++ 04_whileLoop/01_whileloop.py
@@ -11,11 +11,33 @@
 #     i+=1
 # print(total)
     
-i=1;
-fact =1;
+# i=1;
+# fact =1;
 
-while i <= 5 :
-    fact = fact*i;
-    i=i+1;
-    print(fact)
-print(fact)
+# while i <= 5 :
+#     fact = fact*i;
+#     i=i+1;
+#     print(fact)
+# print(fact)
+
+
+# 
+
+# i=2;
+# while i <=100:
+#     print(i)
+#     i+=2
+
+# n = int(input("Enter Number: "))
+
+i=1;
+# while i <=n:
+#     print(i);
+#     i+=1
+
+while True:
+    print(i)
+    if i ==10:
+        break
+    i+=1
+   
